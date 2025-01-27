bfc558d310bdb44cb90fa554c0bac35f1fcbd1cb
BavyaSri2004 <164765611+BavyaSri2004@users.noreply.github.com>
2025-01-27 21:10:46 +0530

    Update Number guess.cpp

--- Number guess.cpp
+++ Number guess.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+using namespace std;
 
+int main() {
+    srand(time(0)); // Seed the random number generator with the current time
+    int randomNumber = rand() % 100 + 1; // Generate a random number between 1 and 100
+    int guess;
+
+    cout << "Welcome to the Number Guessing Game!" << endl;
+    cout << "I have selected a number between 1 and 100. Can you guess it?" << endl;
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+
+        if (guess > randomNumber) {
+            cout << "Too high! Try again." << endl;
+        } else if (guess < randomNumber) {
+            cout << "Too low! Try again." << endl;
+        } else {
+            cout << "Congratulations! You guessed the correct number: " << randomNumber << endl;
+        }
+    } while (guess != randomNumber);
+
+    return 0;
+}
